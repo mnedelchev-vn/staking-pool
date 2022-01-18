9f9efa22fb1f5b1df40bf30c1a17aa0711843759
unknown <miroslav.nedelchev@dentacoin.com>
2022-01-18 21:21:39 +0200

    push

--- StakingProgram.sol
+++ StakingProgram.sol
@@ -1,3 +1,5 @@
+// Dentacoin Foundation
+
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.0;
 
@@ -89,21 +91,21 @@ contract StakingProgram is Ownable, SafeMath {
     uint256 public unstakingFee; // percentage
     uint256 public round = 1;
     uint256 public totalStakes = 0;
-    uint256 public totalFees = 0; // not needed
+    uint256 public totalFees = 0; // TEMP VARIABLE, TO BE REMOVED
     uint256 public totalDividends = 0;
     uint256 public scaling = 10 ** 10;
     bool public stakingStopped = false;
     address public acceleratorAddress = address(0);
-    
+
     struct Staker {
         uint256 stakedTokens;
         uint256 round;
         uint256 remainder;
     }
-    
+
     mapping(address => Staker) public stakers;
     mapping(uint256 => uint256) public payouts;
-    
+
     constructor(address _erc20token_address, uint256 _stakingFee, uint256 _unstakingFee) {
         erc20tokenInstance = ERC20token(_erc20token_address);
         stakingFee = _stakingFee;
@@ -114,7 +116,7 @@ contract StakingProgram is Ownable, SafeMath {
     event staked(address staker, uint256 tokens, uint256 fee);
     event unstaked(address staker, uint256 tokens, uint256 fee);
     event payout(uint256 round, uint256 tokens, address sender);
-    event claimedReward(address staker, uint256 reward); 
+    event claimedReward(address staker, uint256 reward);
     // ==================================== /EVENTS ====================================
 
     // ==================================== MODIFIERS ====================================
@@ -144,35 +146,35 @@ contract StakingProgram is Ownable, SafeMath {
     }
 
     function setAcceleratorAddress(address _address) external onlyOwner {
-        acceleratorAddress = _address;
+        acceleratorAddress = address(_address);
     }
     // ==================================== /CONTRACT ADMIN ====================================
-    
+
     // ==================================== CONTRACT BODY ====================================
     function stake(uint256 _tokens_amount) external checkIfStakingStopped {
         require(_tokens_amount > 0, "Invalid token amount.");
         require(erc20tokenInstance.transferFrom(msg.sender, address(this), _tokens_amount), "Tokens cannot be transferred from sender.");
-        
+
         uint256 _fee = 0;
         if (totalStakes  > 0) {
             // calculating this user staking fee based on the tokens amount that user want to stake
             _fee = div(mul(_tokens_amount, stakingFee), 100);
-			_addPayout(_fee);
+            _addPayout(_fee);
         }
-		
-		// if staking not for first time this means that there are already existing rewards
-		uint256 existingRewards = getPendingReward(msg.sender);
-		if (existingRewards > 0) {
-			stakers[msg.sender].remainder = add(stakers[msg.sender].remainder, existingRewards);
-		}
-        
+
+        // if staking not for first time this means that there are already existing rewards
+        uint256 existingRewards = getPendingReward(msg.sender);
+        if (existingRewards > 0) {
+            stakers[msg.sender].remainder = add(stakers[msg.sender].remainder, existingRewards);
+        }
+
         // saving user staked tokens minus the staking fee
         stakers[msg.sender].stakedTokens = add(sub(_tokens_amount, _fee), stakers[msg.sender].stakedTokens);
         stakers[msg.sender].round = round;
-        
+
         // adding this user stake to the totalStakes
         totalStakes = add(totalStakes, sub(_tokens_amount, _fee));
-        
+
         emit staked(msg.sender, sub(_tokens_amount, _fee), _fee);
     }
 
@@ -180,84 +182,84 @@ contract StakingProgram is Ownable, SafeMath {
         require(acceleratorAddress != address(0), "Invalid address.");
         require(_tokens_amount > 0, "Invalid token amount.");
         require(erc20tokenInstance.transferFrom(msg.sender, address(this), _tokens_amount), "Tokens cannot be transferred from sender.");
-        
+
         uint256 _fee = 0;
         if (totalStakes  > 0) {
             // calculating this user staking fee based on the tokens amount that user want to stake
             _fee = div(mul(_tokens_amount, stakingFee), 100);
-			_addPayout(_fee);
+            _addPayout(_fee);
         }
-		
-		// if staking not for first time this means that there are already existing rewards
-		uint256 existingRewards = getPendingReward(_staker);
-		if (existingRewards > 0) {
-			stakers[_staker].remainder = add(stakers[_staker].remainder, existingRewards);
-		}
-        
+
+        // if staking not for first time this means that there are already existing rewards
+        uint256 existingRewards = getPendingReward(_staker);
+        if (existingRewards > 0) {
+            stakers[_staker].remainder = add(stakers[_staker].remainder, existingRewards);
+        }
+
         // saving user staked tokens minus the staking fee
         stakers[_staker].stakedTokens = add(sub(_tokens_amount, _fee), stakers[_staker].stakedTokens);
         stakers[_staker].round = round;
-        
+
         // adding this user stake to the totalStakes
         totalStakes = add(totalStakes, sub(_tokens_amount, _fee));
-        
+
         emit staked(_staker, sub(_tokens_amount, _fee), _fee);
     }
 
     function claimReward() external {
         uint256 pendingReward = getPendingReward(msg.sender);
         if (pendingReward > 0) {
-            stakers[msg.sender].remainder = 0;        
+            stakers[msg.sender].remainder = 0;
             stakers[msg.sender].round = round; // update the round
-        
+
             require(erc20tokenInstance.transfer(msg.sender, pendingReward), "ERROR: error in sending reward from contract to sender.");
-            
+
             emit claimedReward(msg.sender, pendingReward);
         }
     }
-    
+
     function unstake(uint256 _tokens_amount) external {
         require(_tokens_amount > 0 && stakers[msg.sender].stakedTokens >= _tokens_amount, "Invalid token amount to unstake.");
-        
+
         stakers[msg.sender].stakedTokens = sub(stakers[msg.sender].stakedTokens, _tokens_amount);
         stakers[msg.sender].round = round;
 
         // calculating this user unstaking fee based on the tokens amount that user want to unstake
         uint256 _fee = div(mul(_tokens_amount, unstakingFee), 100);
-        
+
         // sending to user desired token amount minus his unstacking fee
         require(erc20tokenInstance.transfer(msg.sender, sub(_tokens_amount, _fee)), "Error in unstaking tokens.");
-        
+
         totalStakes = sub(totalStakes, _tokens_amount);
         if (totalStakes > 0) {
-			_addPayout(_fee);
+            _addPayout(_fee);
         }
-        
+
         emit unstaked(msg.sender, sub(_tokens_amount, _fee), _fee);
     }
-    
+
     function addRewards(uint256 _tokens_amount) external checkIfStakingStopped {
         require(erc20tokenInstance.transferFrom(msg.sender, address(this), _tokens_amount), "Tokens cannot be transferred from sender.");
         _addPayout(_tokens_amount);
     }
-	
-	function _addPayout(uint256 _fee) private {
-		uint256 dividendPerToken = div(mul(_fee, scaling), totalStakes);
-		totalDividends = add(totalDividends, dividendPerToken); 
-		payouts[round] = add(payouts[round-1], dividendPerToken);
-		
-		// adding this user fee to the totalFees
-		totalFees = add(totalFees, _fee);
-		round+=1;
-        
+
+    function _addPayout(uint256 _fee) private {
+        uint256 dividendPerToken = div(mul(_fee, scaling), totalStakes);
+        totalDividends = add(totalDividends, dividendPerToken);
+        payouts[round] = add(payouts[round-1], dividendPerToken);
+
+        // adding this user fee to the totalFees
+        totalFees = add(totalFees, _fee);
+        round+=1;
+
         emit payout(round, _fee, msg.sender);
-	}
-    
+    }
+
     function getPendingReward(address _staker) public view returns(uint256) {
         uint256 amount = mul((sub(totalDividends, payouts[stakers[_staker].round - 1])), stakers[_staker].stakedTokens);
         return add(div(amount, scaling), stakers[_staker].remainder);
     }
-    
+
     function getUserStake(address _address) public view returns(uint256) {
         return stakers[_address].stakedTokens;
     }
@@ -268,3 +270,5 @@ interface ERC20token {
     function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
     function transfer(address _to, uint256 _value) external returns (bool success);
 }
+
+// MN bby ¯\_(ツ)_/¯
